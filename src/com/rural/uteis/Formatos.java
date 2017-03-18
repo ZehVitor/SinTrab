c253665837f2fe53c934ec5279ba2a6a60d67041
diegonndnn <javanatal@gmail.com>
2017-03-18 12:35:32 -0300

    Classe Formatos

---
+++ src/com/rural/uteis/Formatos.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rural.uteis;
+
+import java.text.ParseException;
+import javax.swing.text.MaskFormatter;
+
+/**
+ *
+ * @author diego
+ */
+public class Formatos {
+
+    public MaskFormatter getFormatoDataSimples() throws ParseException {
+        MaskFormatter m = new MaskFormatter("##/##/####");
+        return m;
+    }
+
+    public MaskFormatter getFormatoCPF() throws ParseException {
+        MaskFormatter m = new MaskFormatter("###.###.###-###");
+        return m;
+    }
+
+}
