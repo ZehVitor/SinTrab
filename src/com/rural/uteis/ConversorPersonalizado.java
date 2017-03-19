08e17902f5934923f427b49ef07e3a1591cdbe56
diegonndnn <javanatal@gmail.com>
2017-03-18 23:28:32 -0300

    Refatoração de Entidade Associado, ConversorPersonalizado e internal frame associado.

--- src/com/rural/uteis/ConversorPersonalizado.java
+++ src/com/rural/uteis/ConversorPersonalizado.java
@@ -5,6 +5,7 @@
  */
 package com.rural.uteis;
 
+import com.rural.enums.Sexo;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -17,6 +18,13 @@ public class ConversorPersonalizado {
     static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public static LocalDate convertStringToLocalDate(String textoData) {
+        String aux = textoData;
+        aux = aux.replaceAll("/", "");
+        aux = aux.trim();
+
+        if (aux.equals("")) {
+            return null;
+        }
         return LocalDate.parse(textoData, dtf);
     }
 
@@ -25,10 +33,19 @@ public class ConversorPersonalizado {
     }
 
     public static String convertNumberToString(Long numero) {
+      
+        if(numero.equals("")){
+            return null;
+        }
+               
         return String.valueOf(numero);
     }
 
     public static String convertNumberToString(Integer numero) {
         return String.valueOf(numero);
+    }
+
+    public static Sexo convertStringToSexo(String texto) {
+        return Sexo.valueOf(texto);
     }
 }
